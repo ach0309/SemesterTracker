c5f98fbcce14ad88fa9fccbe92a97f049f24cc2f
aeonchavez <71860250+aeonchavez@users.noreply.github.com>
2020-09-24 22:10:52 -0400

    Add files via upload

---
+++ semTracker/percent.py
@@ -0,0 +1,18 @@
+from assign import *
+from exams import *
+from projects import *
+from quiz import *
+
+class Percent:
+    def __init__(self, Phw, Pproj, Pexam, Pquiz): #totalgrade of each
+        self.Phw = Phw
+        self.Pproj = Pproj
+        self.Pexam = Pexam
+        self.Pquiz = Pquiz
+
+
+
+
+
+
+
---
+++ semTracker/quiz.py
@@ -0,0 +1,19 @@
+class Quiz:
+    def __init__(self, num, grade):
+        self.num = num
+        self.grade = grade
+
+    def assignInfo(self):
+        a_info = "Exam #" + str(self.num) + ": " + str(self.grade) + "%"
+        return a_info
+
+    def __str__(self):
+        return str(self.grade)
+
+    def toPercent(self, score, total):
+        grade = ( score/total ) * 100
+        return "{:.2f}".format(grade)
+
+
+
+
